6872f10de7fed6c20d35910eee44484bd7b52ca5
hojeong2747 <hojeong2747@gmail.com>
2023-05-09 23:27:34 +0900

    add/jpashop: JPA 활용 2 섹션1

---
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,82 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController // Controller + ResponseBody
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream().map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { 
+        // RequestBody : json 으로 온 body 를 Member 에 mapping 해서 바꿔줌
+        // @Valid 써서 null 일 때 생성 안 되게 막아줌. Member 의 validation 검사
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    private class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Data
+    private class CreateMemberRequest {
+        @NotEmpty // 여기에 넣을 수 있음
+        private String name;
+    }
+}
--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +17,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+//    @NotEmpty // presentation 계층을 위한 검증 로직이 엔티티에 있는 것 -> api 마다 다른 조건이 될 수 있음. & 엔티티 변경 시 api 스펙이 바뀜
     private String name;
 
     @Embedded
